e68297eecb9d8d68ef91cf2731513cc85bb0d61e
Louzada08 <valuz.anderson.to@gmail.com>
2020-03-10 17:53:58 -0300

    Starting developent app api

---
+++ Invoicing/Environment/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Environment
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 22 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
